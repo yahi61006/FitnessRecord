f6d2c26ef068727bf227698430b6294a9ac3088b
yahi61006 <yahi61006@gmail.com>
2019-11-27 14:55:46 +0800

    Initial Commit

---
+++ FitnessRecord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessRecord
+//
+//  Created by Kumo on 2019/11/27.
+//  Copyright © 2019 Kumo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
